e1c63e0c5f06978d1559afa0dde6aa56a1fd950c
a.breezy <ambrose.wilkins@gmail.com>
2022-02-03 20:06:35 -0500

    update pizza model and pizza routes

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(require('./routes'));
+app.use(require("./routes"));
+
+// tells mongoose which db to connect to
+// if MONGODB_URI (like on heroku)use that, otehrwise use local server
+mongoose.connect(
+	process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
+);
+
+// use this to log mongo queries being executed
+mongoose.set("debug", true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
